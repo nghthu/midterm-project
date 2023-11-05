f3ffa2981eec0a976c7df7c6035d15de842e71dc
nghthu <hanhh.thu@gmail.com>
2023-11-05 23:15:12 +0700

    add dashboard page interface

---
+++ app/(user)/dashboard/page.js
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import Button from "@/components/UI/Button";
+import Input from "@/components/UI/Input";
+
+const Dashboard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logout}>
+        <Link href="/">
+          <Button className={styles.signin}>Log out</Button>
+        </Link>
+      </div>
+      <h1>Hello, Username!</h1>
+      <div className={styles.card}>
+        <form className={styles.form}>
+          <p className={styles["edit-label"]}>Edit your Profile</p>
+          <div className={styles["input-fields"]}>
+            <div className={styles.column}>
+              <p className={styles.tag}>Full Name</p>
+              <input type="text" />
+              <p className={styles.tag}>Email</p>
+              <input type="email" />
+              <p className={styles.tag}>Phone Number</p>
+              <input type="text" />
+            </div>
+            <div className={styles.column}>
+              <p className={styles.tag}>Password</p>
+              <input type="password" />
+              <p className={styles.tag}>Adress</p>
+              <input type="text" />
+              <div className={styles["radio-group"]}>
+                <label className={styles["radio-container"]}>
+                  Male
+                  <input type="radio" name="radio" />
+                  <span className={styles.checkmark}></span>
+                </label>
+                <label className={styles["radio-container"]}>
+                  Female
+                  <input type="radio" name="radio" />
+                  <span className={styles.checkmark}></span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <Button>Update info</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;

6f1a2e2bb7449cf9bf64bfffd750b32280726dbc
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-08 02:40:53 +0700

    using user & auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import authRouter from "./src/routes/authRoute";
+import userRouter from "./src/routes/userRoute";
 
 const app = express();
 const PORT = 3000;
@@ -6,12 +8,15 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use(authRouter);
+app.use(userRouter);
+
 // Custom error handler
 app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
-
   const errStatus = err.statusCode || 500;
   res.status(errStatus).json({
     error: err,
@@ -19,5 +24,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}/`);
 });

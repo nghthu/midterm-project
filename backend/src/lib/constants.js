bcf87b0f59aabe8dbdcf769bd3a816d68223deca
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-08 02:40:06 +0700

    change name

--- backend/src/lib/constants.js
+++ backend/src/lib/constants.js
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 export const ERROR_INVALID_INPUT = "Invalid Input";
 export const ERROR_EMAIL_ALREADY_EXISTS = "Email Already Exists";
 export const ERROR_WRONG_EMAIL = "Wrong Email/Email Does Not Exist";
@@ -6,5 +8,5 @@ export const MESSAGE_SIGNUP_SUCCESSFULLY = "Sign Up Successfully";
 export const JWT_ERROR_TOKEN_EXPIRED = "TokenExpiredError";
 export const SALT_ROUNDS = 10;
 
-export const { DB_URI, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } =
+export const { DATABASE_URL, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } =
   process.env;

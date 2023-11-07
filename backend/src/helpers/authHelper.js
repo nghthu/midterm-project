05e59be5d55a99127a562d63164b8f16fdd8af4a
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-08 02:42:33 +0700

    returning token

--- backend/src/helpers/authHelper.js
+++ backend/src/helpers/authHelper.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from "../lib/constants";
 
 const genAccessToken = (payload) => {
-  jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
     expiresIn: "15m",
   });
 };
 
 const genRefreshToken = (payload) => {
-  jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
     expiresIn: "7d",
   });
 };

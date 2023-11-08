8184fba27b5e93019c2e9c5b1118248dc5c00776
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-09 02:05:53 +0700

    remove timestamp & change to camel case

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,39 +1,47 @@
 const initUser = (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      unique: true,
-      primaryKey: true,
+  const User = sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "full_name",
+      },
+      tel: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      refreshToken: {
+        type: DataTypes.TEXT,
+        field: "refresh_token",
+      },
+      sex: {
+        type: DataTypes.STRING,
+      },
+      address: {
+        type: DataTypes.TEXT,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    full_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    tel: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    refresh_token: {
-      type: DataTypes.TEXT,
-    },
-    sex: {
-      type: DataTypes.STRING,
-    },
-    address: {
-      type: DataTypes.TEXT,
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
 
   return User;
 };

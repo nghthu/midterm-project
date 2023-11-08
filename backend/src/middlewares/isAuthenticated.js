e0b992eed713222c31223edb51d67c98e946dd5b
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-09 02:04:10 +0700

    handle throwing error

--- backend/src/middlewares/isAuthenticated.js
+++ backend/src/middlewares/isAuthenticated.js
@@ -5,17 +5,30 @@ const isAuthenticated = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
 
   // Verify access token
-  const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
+  const result = jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      if (err.name == JWT_ERROR_TOKEN_EXPIRED) {
-        return res.redirect(401, "/refresh-token");
+      if (err.name === JWT_ERROR_TOKEN_EXPIRED) {
+        return {
+          status: 401,
+          error: err,
+        };
       }
-      console.error(err);
+      return {
+        status: 400,
+        error: err,
+      };
     }
-    return decoded;
+    return {
+      status: 200,
+      decoded,
+    };
   });
+  if (result.status !== 200) {
+    res.status(result.status);
+    throw new Error(result.error);
+  }
 
-  req.user = { id: decoded.id };
+  req.user = { id: result.decoded.id };
   next();
 };
 

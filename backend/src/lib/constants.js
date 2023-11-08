6a580d7ae54f54787587ec10119f670f935b4450
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-09 02:06:34 +0700

    new errors

--- backend/src/lib/constants.js
+++ backend/src/lib/constants.js
@@ -2,8 +2,9 @@ import "dotenv/config";
 
 export const ERROR_INVALID_INPUT = "Invalid Input";
 export const ERROR_EMAIL_ALREADY_EXISTS = "Email Already Exists";
-export const ERROR_WRONG_EMAIL = "Wrong Email/Email Does Not Exist";
-export const ERROR_WRONG_PASSWORD = "Wrong Password";
+export const ERROR_UNKNOWN = "Unknown Error Occurs";
+export const ERROR_WRONG_CREDENTIALS = "No User Found For This Email/Password";
+export const ERROR_BAD_REQUEST = "Bad Request";
 export const MESSAGE_SIGNUP_SUCCESSFULLY = "Sign Up Successfully";
 export const JWT_ERROR_TOKEN_EXPIRED = "TokenExpiredError";
 export const SALT_ROUNDS = 10;

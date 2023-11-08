eea243252176b5f5a5484629323094baa186d602
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-09 02:08:08 +0700

    handle error & add routes' prefix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import "express-async-errors";
 import authRouter from "./routes/authRoute";
 import userRouter from "./routes/userRoute";
 
@@ -11,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use(authRouter);
-app.use(userRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello world!" });
@@ -23,9 +24,10 @@ app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
-  const errStatus = err.statusCode || 500;
-  res.status(errStatus).json({
-    error: err,
+  const status = res.statusCode || 500;
+  console.error(err);
+  res.status(status).json({
+    error: err.message,
   });
 });
 

f3ce4ddd2ef789e3c273720160f1831b3e0a4432
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-11-09 09:26:26 +0700

    handle error & hash password in edit profile

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,3 +1,7 @@
+import {
+  ERROR_FULLNAME_NOT_EMPTY,
+  ERROR_REQUIRE_ALL_FIELDS,
+} from "../lib/constants";
 import { User } from "../models";
 
 const getProfile = async (req, res) => {
@@ -9,8 +13,44 @@ const getProfile = async (req, res) => {
 
 const editProfile = async (req, res) => {
   const profile = req.body;
+  if (
+    !profile?.fullName ||
+    !profile?.email ||
+    !profile?.tel ||
+    !profile?.sex ||
+    !profile?.address ||
+    !profile?.password
+  ) {
+    res.status(400);
+    throw new Error(ERROR_REQUIRE_ALL_FIELDS);
+  }
+  if (profile.fullName === "") {
+    res.status(400);
+    throw new Error(ERROR_FULLNAME_NOT_EMPTY);
+  }
+  if (profile.email === "") {
+    res.status(400);
+    throw new Error(ERROR_EMAIL_NOT_EMPTY);
+  }
+  if (profile.password === "") {
+    res.status(400);
+    throw new Error(ERROR_PASSWORD_NOT_EMPTY);
+  }
+  if (profile.tel === "") {
+    res.status(400);
+    throw new Error(ERROR_TEL_NOT_EMPTY);
+  }
+
   const user = await User.findByPk(req.user.id);
-  await user.update({ ...profile });
+  const hashedPw = await bcrypt.hash(profile.password, SALT_ROUNDS);
+  await user.update({
+    email: profile.email,
+    fullName: profile.fullName,
+    tel: profile.tel,
+    sex: profile.sex,
+    address: profile.address,
+    password: hashedPw,
+  });
   return res.json({
     email: user.email,
     fullName: user.fullName,
